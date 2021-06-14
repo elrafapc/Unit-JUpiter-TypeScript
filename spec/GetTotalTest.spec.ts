600aa811ec6802f4bbe164fec4bb3a79166e163a
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-13 22:10:07 -0300

    Finishing Configurations to run test

--- spec/GetTotalTest.spec.ts
+++ spec/GetTotalTest.spec.ts
@@ -14,16 +14,4 @@ describe("Testing Functions From ShoppingCart", () =>{
 
         expect(cart.getTotal()).toBe(30);
     })
-
-    it("Verifying the total Value from cart", () =>{
-        const b1: Book = new Book("Book1", 10, "1");
-        const b2: Book = new Book("Book2", 20, "2");
-
-        const cart: ShoppingCart = new ShoppingCart();
-
-        cart.addBook(b1);
-        cart.addBook(b2);
-
-        expect(cart.getTotal()).toBe(25);
-    })
 })
