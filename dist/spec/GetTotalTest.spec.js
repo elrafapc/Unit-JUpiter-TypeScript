ca735c5ad334413e6854e7096a1fa4dd06a9b4e7
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-13 22:10:51 -0300

    Finishing Configurations to run test

--- dist/spec/GetTotalTest.spec.js
+++ dist/spec/GetTotalTest.spec.js
@@ -11,13 +11,5 @@ describe("Testing Functions From ShoppingCart", function () {
         cart.addBook(b2);
         expect(cart.getTotal()).toBe(30);
     });
-    it("Verifying the total Value from cart", function () {
-        var b1 = new Book_1.Book("Book1", 10, "1");
-        var b2 = new Book_1.Book("Book2", 20, "2");
-        var cart = new ShoppingCart_1.ShoppingCart();
-        cart.addBook(b1);
-        cart.addBook(b2);
-        expect(cart.getTotal()).toBe(25);
-    });
 });
 //# sourceMappingURL=GetTotalTest.spec.js.map
