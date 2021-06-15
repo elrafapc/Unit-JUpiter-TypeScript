81a7c9718d9f2e881b06d832e3b45389244a9fcb
Rafael Carneiro <rafael.carneiro@mereo.com>
2021-06-14 21:20:16 -0300

    add new cucumber test

--- features/steps/divideSteps.ts
+++ features/steps/divideSteps.ts
@@ -7,7 +7,7 @@ let b1: Book = null;
 let b2: Book = null;
 let cart: ShoppingCart = null;
 
-Given("I choose some books", function(){
+Given("I choose two books", function(){
     b1 = new Book("Book1", 10, "1");
     b2 = new Book("Book2", 20, "2");
 });
@@ -23,3 +23,8 @@ When("I add these books to the cart", function(){
 Then("the total must be updated", function(){
     expect(cart.getTotal()).to.be.eq(30);
 });
+
+Then("the count of items must be 2", function(){
+    expect(cart.countItems()).to.be.eq(2);
+});
+
